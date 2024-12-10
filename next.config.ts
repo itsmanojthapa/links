8d73069d191daf5a522c66ad58c0d69259a8adab
itsmanojthapa <itsmanojthapa@gmail.com>
2024-12-11 01:04:39 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  api: {
-    bodyParser: {
-      sizeLimit: "1mb",
-    },
-  },
 };
 
 export default nextConfig;
